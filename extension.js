92e3d3722f93d9634f864db33767270c20da022d
Frederic de Zorzi <f@idez.net>
2022-04-22 22:57:14 +0200

    init

---
+++ extension.js
@@ -0,0 +1,74 @@
+const { GLib, Gio, GObject, St, Shell } = imports.gi;
+const Clutter = imports.gi.Clutter;
+const Lang = imports.lang;
+const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const Panel = imports.ui.panel;
+const PanelMenu = imports.ui.panelMenu;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+const MESSAGE_KEY = "message";
+
+let SimpleMessage = GObject.registerClass(
+  class SimpleMessage extends PanelMenu.Button {
+    _init() {
+      super._init(0.0, "SimpleMessage");
+      //this.parent(St.Align.START);
+      this._settings = Convenience.getSettings();
+      this._settingsChangedSignal = this._settings.connect(
+        "changed",
+        Lang.bind(this, function () {
+          this._refreshUI();
+        })
+      );
+      this._buildUI();
+      this._refreshUI();
+    }
+
+    // _onDestroy() {
+    //   this._settings.disconnect(this._settingsChangedSignal);
+    //   this.parent();
+    // }
+
+    _buildUI() {
+      this._message = new St.Label({
+        name: "simple-message-message",
+        y_align: Clutter.ActorAlign.CENTER,
+        y_expand: true,
+      });
+      (this instanceof Clutter.Actor ? this : this.actor).add_actor(
+        this._message
+      );
+      this._newMessage = new St.Entry({
+        name: "simple-message-new-message",
+        track_hover: true,
+        can_focus: true,
+      });
+    }
+
+    _refreshUI() {
+      let message = this._loadMessage();
+      this._message.set_text(message);
+      this._newMessage.set_text(message);
+    }
+
+    _loadMessage() {
+      return this._settings.get_string(MESSAGE_KEY);
+    }
+  }
+);
+
+let simpleMessage;
+
+function init() {}
+
+function enable() {
+  simpleMessage = new SimpleMessage();
+  Main.panel.addToStatusArea("simpleMessage", simpleMessage);
+}
+
+function disable() {
+  if (simpleMessage != null) simpleMessage.destroy();
+}

7b01e55fd030867815b7352ce7fe2eca328e017a
Frederic de Zorzi <f@idez.net>
2024-04-13 08:03:23 +0200

    reformat

--- extension.js
+++ extension.js
@@ -15,122 +15,138 @@
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-'use strict';
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+"use strict";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 
-import Clutter from 'gi://Clutter';
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import GLib from 'gi://GLib';
+import Clutter from "gi://Clutter";
+import GObject from "gi://GObject";
+import St from "gi://St";
+import GLib from "gi://GLib";
 let simpleMessage = null; // Variable to hold our extension
 
-
 export default class SimpleMessageExtension extends Extension {
-    /**
-     * This class is constructed once when your extension is loaded, not
-     * enabled. This is a good time to setup translations or anything else you
-     * only do once.
-     *
-     * You MUST NOT make any changes to GNOME Shell, connect any signals or add
-     * any event sources here.
-     *
-     * @param {ExtensionMeta} metadata - An extension meta object
-     */
-    constructor(metadata) {
-        super(metadata);
-    }
+  /**
+   * This class is constructed once when your extension is loaded, not
+   * enabled. This is a good time to setup translations or anything else you
+   * only do once.
+   *
+   * You MUST NOT make any changes to GNOME Shell, connect any signals or add
+   * any event sources here.
+   *
+   * @param {ExtensionMeta} metadata - An extension meta object
+   */
+  constructor(metadata) {
+    super(metadata);
+  }
 
-    /**
-     * This function is called when your extension is enabled, which could be
-     * done in GNOME Extensions, when you log in or when the screen is unlocked.
-     *
-     * This is when you should setup any UI for your extension, change existing
-     * widgets, connect signals or modify GNOME Shell's behavior.
-     */
-    enable() {
-        simpleMessage = new SimpleMessage();
-    }
+  /**
+   * This function is called when your extension is enabled, which could be
+   * done in GNOME Extensions, when you log in or when the screen is unlocked.
+   *
+   * This is when you should setup any UI for your extension, change existing
+   * widgets, connect signals or modify GNOME Shell's behavior.
+   */
+  enable() {
+    simpleMessage = new SimpleMessage();
+  }
 
-    /**
-     * This function is called when your extension is uninstalled, disabled in
-     * GNOME Extensions or when the screen locks.
-     *
-     * Anything you created, modified or setup in enable() MUST be undone here.
-     * Not doing so is the most common reason extensions are rejected in review!
-     */
-    disable() {
-        simpleMessage.destroy();
-        simpleMessage = null;
-    }
+  /**
+   * This function is called when your extension is uninstalled, disabled in
+   * GNOME Extensions or when the screen locks.
+   *
+   * Anything you created, modified or setup in enable() MUST be undone here.
+   * Not doing so is the most common reason extensions are rejected in review!
+   */
+  disable() {
+    simpleMessage.destroy();
+    simpleMessage = null;
+  }
 }
 
-
-
-
-
-
 // Create the extension object
-let SimpleMessage = GObject.registerClass({GTypeName: 'SimpleMessage'}, class SimpleMessage extends PanelMenu.Button {
+let SimpleMessage = GObject.registerClass(
+  { GTypeName: "SimpleMessage" },
+  class SimpleMessage extends PanelMenu.Button {
     _init() {
-        super._init(/*St.Align.START*/);
+      super._init(/*St.Align.START*/);
 
-        // Getting the extension object by UUID
-        let extensionObject = Extension.lookupByUUID('simple-message@freddez');
-        this._settings = extensionObject.getSettings();
-        //this._settings = ExtensionUtils.getSettings();
-        this.message = this._settings.get_string('message');
-        this.command = this._settings.get_string('command');
-        this.message_alignment = this._settings.get_int('panel-alignment');
-        this.message_position = this._settings.get_int('panel-position');
+      // Getting the extension object by UUID
+      let extensionObject = Extension.lookupByUUID("simple-message@freddez");
+      this._settings = extensionObject.getSettings();
+      //this._settings = ExtensionUtils.getSettings();
+      this.message = this._settings.get_string("message");
+      this.command = this._settings.get_string("command");
+      this.message_alignment = this._settings.get_int("panel-alignment");
+      this.message_position = this._settings.get_int("panel-position");
 
-        // Create the object that holds and displays the message
-        this.messageBox = new St.Label({
-            name: "simple-message-message",
-            y_align: Clutter.ActorAlign.CENTER,
-            y_expand: true,
-        });
-        this.add_actor(this.messageBox);
+      // Create the object that holds and displays the message
+      this.messageBox = new St.Label({
+        name: "simple-message-message",
+        y_align: Clutter.ActorAlign.CENTER,
+        y_expand: true,
+      });
+      this.add_actor(this.messageBox);
 
-        // Add message text
-        this.messageBox.set_text(this.message);
-        // Add message to panel
-        Main.panel.addToStatusArea('simpleMessage', this, this.message_position, this.message_alignment);
+      // Add message text
+      this.messageBox.set_text(this.message);
+      // Add message to panel
+      Main.panel.addToStatusArea(
+        "simpleMessage",
+        this,
+        this.message_position,
+        this.message_alignment,
+      );
 
-        // Connect change in values settings to functions to update the message and position
-        this._settings.connect('changed::message', this._rewriteMessage.bind(this));
-        this._settings.connect('changed::command', this._rewriteCommand.bind(this));
-        this._settings.connect('changed::panel-alignment', this._moveMessage.bind(this));
-        this._settings.connect('changed::panel-position', this._moveMessage.bind(this));
-        this.connect('button-press-event', () => {
-            if (this.command) {
-                GLib.spawn_command_line_async(this.command);
-            }
-        })
+      // Connect change in values settings to functions to update the message and position
+      this._settings.connect(
+        "changed::message",
+        this._rewriteMessage.bind(this),
+      );
+      this._settings.connect(
+        "changed::command",
+        this._rewriteCommand.bind(this),
+      );
+      this._settings.connect(
+        "changed::panel-alignment",
+        this._moveMessage.bind(this),
+      );
+      this._settings.connect(
+        "changed::panel-position",
+        this._moveMessage.bind(this),
+      );
+      this.connect("button-press-event", () => {
+        if (this.command) {
+          GLib.spawn_command_line_async(this.command);
+        }
+      });
     }
 
     _rewriteMessage() {
-        this.message = this._settings.get_string('message');
-        this.messageBox.set_text(this.message);
+      this.message = this._settings.get_string("message");
+      this.messageBox.set_text(this.message);
     }
 
     _moveMessage() {
-        this.get_parent().remove_actor(this);// Remove from the old location
-        this.message_alignment = this._settings.get_int('panel-alignment');
-        this.message_position = this._settings.get_int('panel-position');
+      this.get_parent().remove_actor(this); // Remove from the old location
+      this.message_alignment = this._settings.get_int("panel-alignment");
+      this.message_position = this._settings.get_int("panel-position");
 
-        // Allows easily addressable boxes
-        let boxes = {
-            0: Main.panel._leftBox,
-            1: Main.panel._centerBox,
-            2: Main.panel._rightBox
-        };
-        // Insert at new location
-        boxes[this.message_alignment].insert_child_at_index(this, this.message_position);
+      // Allows easily addressable boxes
+      let boxes = {
+        0: Main.panel._leftBox,
+        1: Main.panel._centerBox,
+        2: Main.panel._rightBox,
+      };
+      // Insert at new location
+      boxes[this.message_alignment].insert_child_at_index(
+        this,
+        this.message_position,
+      );
     }
     _rewriteCommand() {
-        this.command = this._settings.get_string('command');
+      this.command = this._settings.get_string("command");
     }
-
-});
+  },
+);
